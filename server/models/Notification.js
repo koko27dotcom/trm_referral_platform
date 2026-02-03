64dedfc2b5cbed1e391e2fd35cd5aecbafdda0a4
Naw Chu <nawchu@Naws-MacBook-Air.local>
2026-02-03 20:26:08 +0630

    Fix: Remove duplicate export from Notification model

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -428,6 +428,3 @@ NotificationSchema.statics.getByType = function(type, options = {}) {
 const Notification = mongoose.model('Notification', NotificationSchema);
 
 export default Notification;
-
-// Re-export constants for convenience
-export { NOTIFICATION_TYPES, NOTIFICATION_PRIORITY, NOTIFICATION_CHANNELS };
